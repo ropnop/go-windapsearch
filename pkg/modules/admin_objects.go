de038f7c0970fc2bc4bfd183aeade078f51ab0ef
Ronnie Flathers <ronnie.flathersii@motorolasolutions.com>
2020-04-24 19:36:48 -0500

    added admin objects search

---
+++ pkg/modules/admin_objects.go
@@ -0,0 +1,36 @@
+package modules
+
+import (
+	"github.com/ropnop/go-windapsearch/pkg/ldapsession"
+	"github.com/spf13/pflag"
+	"gopkg.in/ldap.v3"
+)
+
+type AdminObjects struct{}
+
+func init() {
+	AllModules = append(AllModules, new(AdminObjects))
+}
+
+func (AdminObjects) Name() string {
+	return "admin-objects"
+}
+
+func (AdminObjects) Description() string {
+	return "Enumerate all objects with protected ACLs (i.e admins)"
+}
+
+func (AdminObjects) FlagSet() *pflag.FlagSet {
+	return pflag.NewFlagSet("adminobjects", pflag.ExitOnError)
+}
+
+func (AdminObjects) DefaultAttrs() []string {
+	return []string{"distinguishedName"}
+}
+
+func (AdminObjects) Run(session *ldapsession.LDAPSession, attrs []string) (*ldap.SearchResult, error) {
+	filter := "(adminCount=1)"
+	sr := session.MakeSimpleSearchRequest(filter, attrs)
+	return session.GetSearchResults(sr)
+}
+
