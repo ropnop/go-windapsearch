76ed74c34fe0bf74b8528013d22c5382787f294a
Ronnie Flathers <ronnie.flathersii@motorolasolutions.com>
2020-04-24 19:14:36 -0500

    add domain admins module

---
+++ pkg/modules/domainadmins.go
@@ -0,0 +1,63 @@
+package modules
+
+import (
+	"fmt"
+	"github.com/ropnop/go-windapsearch/pkg/ldapsession"
+	"github.com/spf13/pflag"
+	"gopkg.in/ldap.v3"
+	"strings"
+)
+
+var DomainAdminGroups = []string{
+"Domain Admins",
+"Domain-Admins",
+"Domain Administrators",
+"Domain-Administrators",
+"Domänen Admins",
+"Domänen-Admins",
+"Domain Admins",
+"Domain-Admins",
+"Domänen Administratoren",
+"Domänen-Administratoren",
+}
+
+type DAModule struct{}
+
+func init() {
+	AllModules = append(AllModules, new(DAModule))
+}
+
+func (DAModule) Name() string {
+	return "das"
+}
+
+func (DAModule) Description() string {
+	return "Recursively list all users objects in Domain Admins group"
+}
+
+func (DAModule) Filter(baseDN string) string {
+	var sb strings.Builder
+	sb.WriteString("(&(objectClass=user)(|")
+	for _, group := range DomainAdminGroups {
+		filter := fmt.Sprintf("(memberof:1.2.840.113556.1.4.1941:=CN=%s,CN=Users,%s)",group,baseDN)
+		sb.WriteString(filter)
+	}
+	sb.WriteString("))")
+	return sb.String()
+}
+
+func (DAModule) FlagSet() *pflag.FlagSet {
+	flags := pflag.NewFlagSet("domain-admins", pflag.ExitOnError)
+	return flags
+}
+
+func (DAModule) DefaultAttrs() []string {
+	return []string{"cn", "sAMAccountName"}
+}
+
+func (d DAModule) Run(session *ldapsession.LDAPSession, attrs []string) (*ldap.SearchResult, error) {
+	filter := d.Filter(session.BaseDN)
+	searchReq := session.MakeSimpleSearchRequest(filter, attrs)
+	return session.GetSearchResults(searchReq)
+}
+
