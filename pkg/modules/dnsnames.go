6fa4f85ddc3745f955f05ff7677ec84a405c505b
Ronnie Flathers <rflathers@marqeta.com>
2020-11-03 11:59:40 -0600

    refactor dnsnames module to use new displayfilters

--- pkg/modules/dnsnames.go
+++ pkg/modules/dnsnames.go
@@ -2,11 +2,11 @@ package modules
 
 import (
 	"fmt"
-	"strings"
-
 	"github.com/go-ldap/ldap/v3"
+	"github.com/ropnop/go-windapsearch/pkg/adschema"
 	"github.com/ropnop/go-windapsearch/pkg/ldapsession"
 	"github.com/spf13/pflag"
+	"strings"
 )
 
 type DnsNamesModule struct{}
@@ -29,52 +29,44 @@ func (d DnsNamesModule) FlagSet() *pflag.FlagSet {
 }
 
 func (d DnsNamesModule) DefaultAttrs() []string {
-	return []string{"dn"}
+	return []string{"name", "dnsTombstoned"}
+}
+
+// Optional function for the module interface that will be called by searchResultWorker
+// This will hide by default a lot of extraneous entries we usually don't care about
+// use '--ignore-display-filters' to bypass this and display everything
+func (d DnsNamesModule) DisplayFilter(entry *adschema.ADEntry) bool {
+	locations := []string{"CN=MicrosoftDNS,DC=DomainDnsZones,%s", "CN=MicrosoftDNS,DC=ForestDnsZones,%s", "CN=MicrosoftDNS,CN=System,%s"}
+	dnContainsFilter := []string{"DC=RootDNSServers", "in-addr.arpa,", "DC=_msdcs", "..TrustAnchors"}
+	dnBeginsFilter := []string{"DC=DomainDnsZones,", "DC=ForestDnsZones,", "DC=_kerberos.", "DC=_ldap.", "DC=_kpasswd.", "DC=_gc.", "DC=@", "DC=_autodiscover."}
+	for _, filterEntry := range dnContainsFilter { // Filter entries like rDNS zones
+		if strings.Contains(entry.DN, filterEntry) {
+			return false
+		}
+	}
+	for _, filterEntry := range dnBeginsFilter { // filter entries used for discovery, e.g. _ldap
+		if strings.HasPrefix(entry.DN, filterEntry) {
+			return false
+		}
+	}
+	for _, filterEntry := range locations { // filter the zone entries themselves
+		if strings.HasPrefix(entry.DN, fmt.Sprintf(filterEntry, "")) {
+			return false
+		}
+	}
+	return true
+
 }
 
 func (d DnsNamesModule) Run(session *ldapsession.LDAPSession, attrs []string) error {
 	locations := []string{"CN=MicrosoftDNS,DC=DomainDnsZones,%s", "CN=MicrosoftDNS,DC=ForestDnsZones,%s", "CN=MicrosoftDNS,CN=System,%s"}
-
-	// I'm too stupid for filtering these entries via LDAP, tried for about a day and failed... now they're implemented here, feel free to improve this
-	dnContainsFilter := []string{"DC=RootDNSServers", "in-addr.arpa,", "DC=_msdcs", "..TrustAnchors"}
-	dnBeginsFilter := []string{"DC=DomainDnsZones,", "DC=ForestDnsZones,", "DC=_kerberos.", "DC=_ldap.", "DC=_kpasswd.", "DC=_gc.", "DC=@", "DC=_autodiscover."}
-	baseDN := session.BaseDN
-	results := make([]*ldap.SearchResult, 0)
+	var searchRequests []*ldap.SearchRequest
 	for _, location := range locations {
-		session.BaseDN = fmt.Sprintf(location, baseDN)
+		dn := fmt.Sprintf(location, session.BaseDN)
 
-		searchReq := session.MakeSimpleSearchRequest("(objectClass=*)", attrs)
-		res, err := session.GetSearchResults(searchReq)
-
-		if err != nil {
-			return err
-		}
-		filteredResults := make([]*ldap.Entry, 0)
-	outer:
-		for _, entry := range res.Entries {
-			for _, filterEntry := range dnContainsFilter { // Filter entries like rDNS zones
-				if strings.Contains(entry.DN, filterEntry) {
-					continue outer
-				}
-			}
-			for _, filterEntry := range dnBeginsFilter { // filter entries used for discovery, e.g. _ldap
-				if strings.HasPrefix(entry.DN, filterEntry) {
-					continue outer
-				}
-			}
-			for _, filterEntry := range locations { // filter the zone entries themselves
-				if strings.HasPrefix(entry.DN, fmt.Sprintf(filterEntry, "")) {
-					continue outer
-				}
-			}
-			filteredResults = append(filteredResults, entry)
-		}
-		res.Entries = filteredResults
-
-		results = append(results, res)
+		searchReq := session.MakeSearchRequestWithDN(dn, "(objectClass=*)", attrs)
+		searchRequests = append(searchRequests, searchReq)
 	}
-	session.BaseDN = baseDN
 
-	session.ManualWriteMultipleSearchResultsToChan(results)
-	return nil
+	return session.ExecuteBulkSearchRequest(searchRequests)
 }
